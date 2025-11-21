347e5d6f9f0f111fc7c9a2ffadf79f8d26d1e38d
medabdelhaye <s.medabdelhay@gmail.com>
2025-11-21 10:52:51 +0000

    first commit

---
+++ demo/src/main/java/com/example/demo/SupNumTd123039Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupNumTd123039Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupNumTd123039Application.class, args);
+	}
+
+}
